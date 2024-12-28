e1385e1954bd45518dd44fee4d05aa5e524d97e4
Yash Chitneni <yash.chitneni@gmail.com>
2024-12-28 11:23:38 -0600

    fix

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: ['style-loader', 'css-loader', 'postcss-loader'],
-    });
-    return config;
-  },
 }
 
 module.exports = nextConfig 
