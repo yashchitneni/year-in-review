54d0398c297ac3cc2ba2917e0a4b95f37000bdea
Yash Chitneni <yash.chitneni@gmail.com>
2024-12-28 11:22:04 -0600

    tailwind

--- next.config.js
+++ next.config.js
@@ -3,6 +3,13 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader', 'postcss-loader'],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig 
